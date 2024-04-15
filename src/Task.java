2bae6821feab8adb419e64092e3fc5c9231b7f13
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-15 14:40:29 +0300

    changes

--- src/Task.java
+++ src/Task.java
@@ -13,19 +13,14 @@ public class Task {
 
     static class DetermineGroup {
         public int determineGroup(int age) {
-            if (age < 7 | 65 < age) {
-                return -1;
-            } else {
-                if (13 >= age) {
-                    return 1;
-                } else {
-                    if (17 >= age)
-                        return 2;
-                    else {
-                        return 3;
-                    }
-                }
-            }
+            if (13 >= age & age >= 7)
+                return 1;
+            if (17 >= age & age >= 14)
+                return 2;
+            if (65 >= age & age >= 18)
+                return 3;
+            else return -1;
         }
     }
 }
+
